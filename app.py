7b4e3cc56d687d1cb8305430448e3417a123518d
kaysha motiani <kayshamotiani2007@gmail.com>
2026-05-28 00:34:09 +0530

    my first own website

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+
 import streamlit as st
+from streamlit_extras.let_it_rain import rain
 
 # Page Setup
 st.set_page_config(
@@ -6,6 +8,33 @@ st.set_page_config(
     page_icon="🎓",
     layout="centered"
 )
+
+theme = st.sidebar.selectbox(
+    "Choose Theme",
+    ["Dark Mode 🌙","Light Mode ☀️"]
+)
+
+if theme == "Dark Mode 🌙":
+
+    st.markdown("""
+    <style>
+    .stApp{
+        background: linear-gradient(135deg,#141E30,#243B55);
+        color:white;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+else:
+
+    st.markdown("""
+    <style>
+    .stApp{
+        background: linear-gradient(135deg,#f6f9fc,#dbeafe);
+        color:black;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 # Title
 st.title("🎓 Student Information Website")
@@ -42,19 +71,28 @@ if st.button("Generate Profile 🚀"):
         **College:** {college}  
         **Course:** {course}  
         **City:** {city}
+        
 
         ### 💬 Personalized Message
 
-        Hello **{name}**!  
-        Wishing you great success in your studies at **{college}**.
-
-        Keep learning, keep growing, and keep building your future! 🚀✨
+        Hello {name}! 🤗 
+        
+        Listen bestie 😉
+        Your GPA is important but your growth matters too.
+        You don't need to have your whole life figured out at {age}.
+        Just keep learning, keep trying, and keep romanticizing your comeback era ✨
+        
+        Wishing you great success in your studies at {college}.
         """)
 
         st.balloons()
-
+        
     else:
         st.warning("Please fill all details.")
+    
+
+
+    
 
 # Footer
 st.markdown("---")
